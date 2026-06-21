fa94bb821ee427e96bfc599a14da06761dd56638
liven <livensagar799@gmail.com>
2026-06-22 01:11:51 +0530

    added variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-person1="Raheem"
-person2="Rohit"
+person1=$1
+person2=$2
 
 echo "$person1: Hi $person2, how are you?"
 echo "$person2: I am good, how about you?"
+echo "$person1: I am doing well, thanks for asking! What have you been up to lately?"
+echo "$person2: I've been busy with DevOps."
