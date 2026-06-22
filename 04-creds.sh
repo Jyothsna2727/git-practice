bc48f8e140f91551ed79796bbcdee9a89baa9f5f
liven <livensagar799@gmail.com>
2026-06-22 23:30:53 +0530

    show date

--- 04-creds.sh
+++ 04-creds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Please enter your username:"
-read username
+read username #takes input, but does not effect above echo statement, so it will always print "Hi" without the username
 
 echo "Hi $username"
