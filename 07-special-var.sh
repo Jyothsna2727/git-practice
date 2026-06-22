f5e4b5df678a84a49c68cb6aedc5c09f8820db4f
liven <livensagar799@gmail.com>
2026-06-22 23:56:13 +0530

    show date

--- 07-special-var.sh
+++ 07-special-var.sh
@@ -3,5 +3,6 @@
 echo "all the variables passed to the code: $@"
 echo "Number of arguments passed: $#"
 echo "Name of the script: $0"
-echo "Current working directory: $pwd"
+echo "Current working directory: $PWD"
 echo "Home directory of current user: $HOME"
+echo "Process ID of present running script: $$"
