88c2eb6635e36b781c0e5b4c0d10603fa41eb6a9
liven <livensagar799@gmail.com>
2026-06-22 23:55:03 +0530

    show date

--- 07-special-var.sh
+++ 07-special-var.sh
@@ -3,3 +3,5 @@
 echo "all the variables passed to the code: $@"
 echo "Number of arguments passed: $#"
 echo "Name of the script: $0"
+echo "Current working directory: $pwd"
+echo "Home directory of current user: $HOME"
